1b228b4a56b10b7845dd4184562d93dde0ecfbe0
anondobormon <anondokpi@gmail.com>
2021-02-23 23:47:55 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let person ={
+    name:  'Dr. Dinesh Chandraw',
+    job: 'Poim'
+  }
+  let person2 = {
+    name: 'Eva Hasan',
+    job: 'Singer'
+  }
+  let style = {
+    color: 'red',
+    backgroundColor: 'yellow'
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +21,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 style={style}>{person.name + ' ' + person.job}</h3>
+        <h3 style={style}>{person2.name + ' ' + person2.job}</h3>
+        
       </header>
     </div>
   );
